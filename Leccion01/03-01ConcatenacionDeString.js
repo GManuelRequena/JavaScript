4cbe5490088f1d8d4c08b8f0aa591daf065d2cc2
Manuel <gmanuelrequena@gmail.com>
2022-07-02 19:42:37 -0300

    Create 03-01ConcatenacionDeString.js

---
+++ Leccion01/03-01ConcatenacionDeString.js
@@ -0,0 +1,17 @@
+//Concatenar cadenas
+var nombre = "Manuel";
+var apellido = "Requena";
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = "Manuel" + " " + "Requena";
+console.log(nombreCompleto2);
+
+var x = nombre + 219 + 2;
+console.log(x);
+
+x = nombre + (2 + 4);
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
