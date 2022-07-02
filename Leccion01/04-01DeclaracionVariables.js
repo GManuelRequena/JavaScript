6292b39c1c1b6172eccb6c5a73aeb74e212fd1f4
Manuel <gmanuelrequena@gmail.com>
2022-07-02 19:47:59 -0300

    Create 04-01DeclaracionVariables.js

---
+++ Leccion01/04-01DeclaracionVariables.js
@@ -0,0 +1,9 @@
+//Var no se recomienda
+//let
+let nombre = "Manuel";
+console.log(nombre);
+
+//constante const
+const apellido = "Requena";
+//apellido = "Rubiolo"; no permite asignar otro valor a apellido
+console.log(apellido);
