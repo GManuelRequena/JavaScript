74859f7355ca54ba20cd6e77e8482dd20c49b21d
Manuel <gmanuelrequena@gmail.com>
2022-07-03 17:27:54 -0300

    Create SwitchSeason.js

---
+++ Leccion01/SwitchSeason.js
@@ -0,0 +1,27 @@
+let month = 3;
+
+switch (month) {
+  case 12:
+  case 1:
+  case 2:
+    console.log("It's Summer");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("It's Fall");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("It's Winter");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("It's Spring");
+    break;
+  default:
+    console.log("Incorrect month");
+    break;
+}
