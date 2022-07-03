60fbadf5e1cc50e26cad0b5aa5b5b0a67fa02176
Manuel <gmanuelrequena@gmail.com>
2022-07-03 16:47:50 -0300

    Create ifUnderAge.js

---
+++ Leccion01/ifUnderAge.js
@@ -0,0 +1,13 @@
+let age = 15,
+  adult = 18;
+
+if (age >= 18) {
+  console.log("The person is an adult.");
+} else {
+  console.log("The person is not an adult.");
+}
+
+//Other way
+age >= 18
+  ? console.log("The person is an adult.")
+  : console.log("The person is not an adult.");
